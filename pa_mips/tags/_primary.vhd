124fdbb92041172cded1b56e702f0aba0eaa0370
Aimar Rodriguez <aimar.rodriguez.s@gmail.com>
2015-01-22 15:33:03 +0100

    Taglist

--- pa_mips/tags/_primary.vhd
+++ pa_mips/tags/_primary.vhd
@@ -2,7 +2,8 @@ library verilog;
 use verilog.vl_types.all;
 entity tags is
     port(
-        addr            : in     vl_logic_vector(31 downto 0);
+        tag             : in     vl_logic_vector(21 downto 0);
+        idx             : in     vl_logic_vector(4 downto 0);
         miss            : out    vl_logic
     );
 end tags;
